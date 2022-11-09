6dbb52438913455271c631ed04489c55f0b72124
Usuario <Usuario@CPD02-M05>
2022-11-09 15:46:22 -0300

    Adicionar arquivos de projeto.

---
+++ _minhascompras__/_minhascompras__/Model/Produto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _minhascompras__.Model
+{
+    internal class Produto
+    {
+    }
+}
---
+++ _minhascompras__/_minhascompras__/View/Listagem.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace _minhascompras__.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Listagem : ContentView
+    {
+        public Listagem()
+        {
+            InitializeComponent();
+        }
+    }
+}
